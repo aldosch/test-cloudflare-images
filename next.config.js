b66004ab47a6da7a67d8542607003f405f9b9a00
aldosch <git@aldo.io>
2024-01-12 15:44:37 +1100

    add imagedelivery.net to images>remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imagedelivery.net",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
